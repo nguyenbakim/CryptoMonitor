afc6612db80428282e1424f13e4792e76ef0d85a
Kim Nguyen <nguyenbakim@gmail.com>
2025-02-22 16:34:07 +0100

    Initial Commit

---
+++ CryptoMonitor/CryptoMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMonitorApp.swift
+//  CryptoMonitor
+//
+//  Created by Kim Nguyen on 22.02.25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoMonitorTests/CryptoMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMonitorTests.swift
+//  CryptoMonitorTests
+//
+//  Created by Kim Nguyen on 22.02.25.
+//
+
+import Testing
+@testable import CryptoMonitor
+
+struct CryptoMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
